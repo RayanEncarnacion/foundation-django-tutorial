95cb6566a145a6c86ea54207af7205264352ac3c
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-03-13 12:35:55 -0400

    feat(Settings): Following django deployment checklist

--- src/foundation/settings.py
+++ src/foundation/settings.py
@@ -15,15 +15,20 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-f51he))ndm&axtxj2fmk3&yxsx5lrmtyyu*ega3#!(@4&_kjgf'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+CSRF_TRUSTED_ORIGINS = ["https://*.railway.app"]
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_AGE = 15 * 60 # 15 mins to expire
+SESSION_SAVE_EVERY_REQUEST = True
 
 ALLOWED_HOSTS = [
     '.railway.app'
@@ -33,7 +38,6 @@ if DEBUG:
         ALLOWED_HOSTS += ['localhost', '127.0.0.1']
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
